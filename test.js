b547eddf852243659e78245e4aab4459533923cd
abhijitkane <abhijit@getpostman.com>
2018-08-07 14:45:49 +0530

    Updating tests, checking for swagger-supported methods

--- test.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs'),
-	Converter = require('./index.js');
-
-var file  = fs.readFileSync('test/data/valid_swagger/sampleswagger.json', 'utf8');
-var jsonObj = JSON.parse(file);
-var pc = Converter.convert(jsonObj);
-
-fs.writeFileSync("postman-collection.json", JSON.stringify(pc.collection,null, 2));
-// console.log('written');
