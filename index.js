5f48d40ed1ee2a0825ee9a1103fb54e5f2c0538a
Zackary Runner <zackrun@microsoft.com>
2018-10-17 01:16:32 -0700

    Add Merge and Collapse functions

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Exports the interface for the plugin
 module.exports = {
 	validate: require('./lib/validate'),
-	convert: require('./lib/convert')
+	convert: require('./lib/convert'),
+	collapse: require('./lib/collapse'),
+	merge: require('./lib/merge')
 };
